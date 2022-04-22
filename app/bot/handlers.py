85f331fa8a2392c5a500ed5bae7734e82f1183e6
PetrShcherbakov <petruxa77799@gmail.com>
2022-04-22 21:04:37 +0600

    Wordle update

--- app/bot/handlers.py
+++ app/bot/handlers.py
@@ -6,4 +6,7 @@ from bot.message_workers import ruby_stick_delete, wordle_delete, sorting_hat
 async def add_handlers(dispatcher):
     dispatcher.register_message_handler(ruby_stick_delete, content_types=types.ContentTypes.STICKER)
     dispatcher.register_message_handler(sorting_hat, commands=['choice_me'])
-    dispatcher.register_message_handler(wordle_delete, [lambda msg: msg.text.lower() == 'cancel'])
+    dispatcher.register_message_handler(wordle_delete, lambda msg: 'wordle' in msg.text.lower() \
+                                                                    or b'\xe2\xac\x9c\xef\xb8\x8f' in msg.text.encode() \
+                                                                    or b'\xf0\x9f\x9f\xa9' in msg.text.encode() \
+                                                                    or b'\xf0\x9f\x9f\xa8' in msg.text.encode())
--- app/bot/message_workers.py
+++ app/bot/message_workers.py
@@ -10,13 +10,8 @@ async def ruby_stick_delete(message: types.Message):
 
 async def wordle_delete(message: types.Message):
     await asyncio.sleep(message.bot.config.DROP_WORDLE_TIMEOUT)
-    text = message.text
-    enc = text.encode()
-    if 'wordle' in text or b'\xe2\xac\x9c\xef\xb8\x8f' in enc or b'\xf0\x9f\x9f\xa9' in enc or b'\xf0\x9f\x9f\xa8' in enc:
-        await message.delete()
+    await message.delete()
 
 
 async def sorting_hat(message: types.Message):
-    resp = await message.bot.aio_session.get('https://statsapi.web.nhl.com/api/v1/game/2021021230/boxscore', ssl=False)
-    # print((await resp.json()).keys())
-    await message.answer((await resp.json()).get('copyright'))
+    pass
